bec5c0b061d07d91caf5dc8ffcd24bbfb3228418
Santi163M <santiagonmartin163@gmail.com>
2023-08-06 13:43:52 -0300

    Register | Estructuración

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
-})
+});
 
-app.use(express.static('public'))
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.post('/register', (req, res) => {
+    res.redirect('/');
+});
+
+app.use(express.static('public'));
 console.log(path.resolve(__dirname, 'public'));
 
-app.listen(3100, () => console.log('Servidor iniciado en el puerto 3100'))
+app.listen(3100, () => console.log('Servidor iniciado en el puerto 3100'));
