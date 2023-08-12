3de7714999cae9103290b8a542a638b8b5351047
Santi163M <santiagonmartin163@gmail.com>
2023-08-12 18:28:19 -0300

    Login finalizado

--- app.js
+++ app.js
@@ -11,10 +11,18 @@ app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/register.html'));
 });
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+
 app.post('/register', (req, res) => {
+    res.redirect('/');
+});
+
+app.post('/login', (req, res) => {
     res.redirect('/');
 });
 
 app.use(express.static('public'));
 
-app.listen(8000, () => console.log('Servidor iniciado en el puerto 3100'));
+app.listen(8000, () => console.log('Servidor iniciado en el puerto 8000'));
